631eeb38dd015fcbe6efb332b8e671832dc22990
André Kadow <andrekadow@gmail.com>
2023-04-19 07:55:54 -0300

    finished

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,35 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
 using TMPro;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class ScoreManager : MonoBehaviour
 {
-    public static ScoreManager Instance;
-    [SerializeField] private TextMeshProUGUI highScoreText;
+    // public static ScoreManager Instance;
+    [SerializeField] private Text highScoreText;
+    private MainManager mainManager;
+    public int highScore;
+    public string theString;
 
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
+    //private void Awake()
+    //{
+    //    if (Instance != null)
+    //    {
+    //        Destroy(gameObject);
+    //        return;
+    //    }
 
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
+    //    Instance = this;
+    //    DontDestroyOnLoad(gameObject);
+    //}
 
 
     // Start is called before the first frame update
     void Start()
     {
-        highScoreText.text = "test";
+        LoadScore();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int highScore;
+        public string recordName;
+    }
+
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/highscore.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            highScore = data.highScore;
+            theString = data.recordName;
+            highScoreText.text = $"Best Score : {theString} : {highScore}";
+        }
     }
 }
